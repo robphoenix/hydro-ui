f6af35c4442f718f455dd0414f8984e0b23e9d09
Rob Phoenix <rob@robphoenix.com>
2019-06-06 16:24:33 +0100

    Filter monitors by category

--- src/components/MonitorsTable.js
+++ src/components/MonitorsTable.js
@@ -1,5 +1,13 @@
-import React, { useState } from 'react'
-import { Table, Pane, majorScale, SegmentedControl, Avatar } from 'evergreen-ui'
+import React, { useState, useEffect } from 'react'
+import {
+  Table,
+  Pane,
+  majorScale,
+  SegmentedControl,
+  Badge,
+  SelectMenu,
+  Button,
+} from 'evergreen-ui'
 
 import MonitorNameCell from './MonitorNameCell'
 import ViewEplQueryCell from './ViewEplQueryCell'
@@ -8,12 +16,25 @@ import MonitorMenuCell from './MonitorMenuCell'
 const MonitorsTable = ({ monitors }) => {
   const [searchQuery, setSearchQuery] = useState('')
   const [monitorsStatus, setMonitorsStatus] = useState('online')
+  const [selectedCategories, setSelectedCategories] = useState([])
+  const [filterCategoriesButtonText, setFilterCategoriesButtonText] = useState(
+    ``,
+  )
 
   const monitorsStatusOptions = [
     { label: 'Online', value: 'online' },
     { label: 'Offline', value: 'offline' },
     { label: 'Archived', value: 'archived' },
   ]
+
+  const categoryOptions = Array.from(
+    new Set(
+      monitors.reduce(
+        (prev, monitor) => [...prev, ...monitor.categories.map((c) => c.name)],
+        [],
+      ),
+    ),
+  ).map((label) => ({ label, value: label }))
 
   const matchesSearchQuery = (monitor) => {
     const query = searchQuery.trim()
@@ -26,23 +47,69 @@ const MonitorsTable = ({ monitors }) => {
     return match && match.length > 0
   }
 
+  const hasSelectedCategories = (categories, selected) => {
+    if (!selected || !selected.length) {
+      return true
+    }
+    return categories
+      .map((category) => category.name)
+      .some((name) => selected.includes(name))
+  }
+
   const filter = (monitors) => {
     return monitors.filter((monitor) => {
-      return monitor.status === monitorsStatus && matchesSearchQuery(monitor)
+      return (
+        monitor.status === monitorsStatus &&
+        matchesSearchQuery(monitor) &&
+        hasSelectedCategories(monitor.categories, selectedCategories)
+      )
     })
   }
+
+  useEffect(() => {
+    let text = `Filter Categories...`
+    const numberOfCategories = selectedCategories.length
+    if (numberOfCategories === 1) {
+      text = selectedCategories
+    } else if (numberOfCategories > 1) {
+      text = `${numberOfCategories} categories selected`
+    }
+    setFilterCategoriesButtonText(text)
+  }, [selectedCategories])
 
   const tableItems = filter(monitors)
 
   return (
     <Pane>
-      <SegmentedControl
-        width={240}
-        options={monitorsStatusOptions}
-        value={monitorsStatus}
-        onChange={setMonitorsStatus}
-        marginBottom={majorScale(4)}
-      />
+      <Pane display="flex">
+        <SegmentedControl
+          width={240}
+          options={monitorsStatusOptions}
+          value={monitorsStatus}
+          onChange={setMonitorsStatus}
+          marginBottom={majorScale(4)}
+          marginRight={majorScale(2)}
+        />
+
+        <SelectMenu
+          isMultiSelect
+          title="Select multiple categories"
+          options={categoryOptions}
+          selected={selectedCategories}
+          onSelect={(item) =>
+            setSelectedCategories([...selectedCategories, item.value])
+          }
+          onDeselect={(item) => {
+            const deselectedItemIndex = selectedCategories.indexOf(item.value)
+            const selectedItems = selectedCategories.filter(
+              (_item, i) => i !== deselectedItemIndex,
+            )
+            setSelectedCategories(selectedItems)
+          }}
+        >
+          <Button>{filterCategoriesButtonText}</Button>
+        </SelectMenu>
+      </Pane>
 
       <Table>
         <Table.Head>
@@ -60,6 +127,17 @@ const MonitorsTable = ({ monitors }) => {
               </Table.Cell>
               <Table.Cell>
                 <ViewEplQueryCell monitor={monitor} />
+              </Table.Cell>
+              <Table.Cell>
+                {monitor.categories.map((category) => (
+                  <Badge
+                    key={category.id}
+                    color="teal"
+                    marginRight={majorScale(1)}
+                  >
+                    {category.name}
+                  </Badge>
+                ))}
               </Table.Cell>
               <Table.Cell justifyContent="flex-end">
                 <MonitorMenuCell monitor={monitor} />
