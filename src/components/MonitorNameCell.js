34dd5aedc39696760b4e5b5acf033f3b631e6dad
Rob Phoenix <rob@robphoenix.com>
2019-06-12 17:36:35 +0100

    Add styled-components

--- src/components/MonitorNameCell.js
+++ src/components/MonitorNameCell.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { Pane, Heading, Text, Link } from 'evergreen-ui'
-import { Link as RouterLink } from '@reach/router'
+import { Pane, Heading, Text, majorScale } from 'evergreen-ui'
+import { Link } from '@reach/router'
+import styled from 'styled-components/macro'
 
 const MonitorNameCell = ({ monitor }) => {
   return (
     <Pane display="flex" flexDirection="column">
-      <Link
-        is={RouterLink}
-        to={`/monitors/${monitor.id}`}
-        textDecoration="none"
-        color="neutral"
-      >
-        <Heading size={600}>{monitor.name}</Heading>
-        <Text size={500}>{monitor.description}</Text>
-      </Link>
+      <Heading size={600} marginBottom={majorScale(2)}>
+        <Link
+          to={`/monitors/${monitor.id}`}
+          css={`
+            text-decoration: none;
+            color: #234361;
+            background-image: linear-gradient(180deg, #d2eef3, #d2eef3);
+            background-repeat: no-repeat;
+            background-position: 0 bottom;
+            background-size: 100% 10%;
+            transition: background-size 0.1s linear;
+
+            &:hover {
+              background-size: 100% 100%;
+            }
+          `}
+        >
+          {monitor.name}
+        </Link>
+      </Heading>
+      <Text size={500}>{monitor.description}</Text>
     </Pane>
   )
 }
--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Link } from 'evergreen-ui'
-import { Link as RouterLink } from '@reach/router'
+import { Link } from '@reach/router'
+import styled from 'styled-components/macro'
 
+// TODO: use evergreen theming to get colour
 const NavLink = (props) => {
   const { to, children } = props
   return (
-    <Link is={RouterLink} to={to} textDecoration="none" color="neutral">
+    <Link
+      to={to}
+      css={`
+        text-decoration: none;
+        color: #425a70;
+      `}
+    >
       {children}
     </Link>
   )
