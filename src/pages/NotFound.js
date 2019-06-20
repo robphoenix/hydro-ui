8da5431d36f0da02b028b0a7dfbcaf92fc2d2de7
Rob Phoenix <rob@robphoenix.com>
2019-06-20 16:26:35 +0100

    Jazz up the 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Pane, Heading, majorScale } from 'evergreen-ui'
+import styled from 'styled-components/macro'
+
+const WavyHeading = styled(Heading)({
+  textDecoration: 'underline',
+  textDecorationStyle: 'wavy',
+  textDecorationColor: '#14B5D0',
+})
 
 const NotFound = () => {
-  return <div>404 motherfucker</div>
+  return (
+    <Pane
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <WavyHeading size={900} marginBottom={majorScale(4)}>
+        404
+      </WavyHeading>
+      <WavyHeading size={900} textTransform="uppercase" letterSpacing="0.4px">
+        page not found
+      </WavyHeading>
+    </Pane>
+  )
 }
 
 export default NotFound
