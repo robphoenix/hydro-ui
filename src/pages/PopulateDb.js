5e3c04a91095ab26d509266dbf3d2ec73c32ee5b
Rob Phoenix <rob@robphoenix.com>
2019-07-08 10:56:07 +0100

    Create categories

--- src/pages/PopulateDb.js
+++ src/pages/PopulateDb.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import { Button, toaster } from 'evergreen-ui'
+import * as faker from 'faker'
 
 import PageContainer from '../components/PageContainer'
 import PageHeading from '../components/PageHeading'
+import { useMonitors } from '../context/monitors-context'
 
 const PopulateDb = () => {
+  const { addCategories } = useMonitors()
+
+  const randomCategories = () => {
+    return Array.from(Array(faker.random.number({ min: 10, max: 20 }))).map(
+      () => {
+        return faker.random.word()
+      },
+    )
+  }
+
+  const createCategories = async () => {
+    const categories = randomCategories()
+    try {
+      await addCategories(categories)
+      toaster.success(`New categories created: ${categories.join(`, `)}`)
+    } catch (error) {
+      toaster.warning(`Error creating categories: ${error}, trying again...`)
+      createCategories()
+    }
+  }
+
   return (
     <PageContainer>
       <PageHeading>Populate Database</PageHeading>
+      <Button onClick={createCategories}>Create categories</Button>
     </PageContainer>
   )
 }
