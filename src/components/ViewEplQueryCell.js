c3190ca3bda226ae7abc4b3be916fe9fa40c68e4
Rob Phoenix <rob@robphoenix.com>
2019-07-11 14:41:57 +0100

    Move content into own component

--- src/components/ViewEplQueryCell.js
+++ src/components/ViewEplQueryCell.js
@@ -11,45 +11,45 @@ import {
 
 import copy from '../utils/copy-to-clipboard'
 
+const Query = ({ monitor, close }) => {
+  return (
+    <Pane
+      width="auto"
+      height="auto"
+      padding={majorScale(2)}
+      background="tint1"
+      display="flex"
+      flexDirection="column"
+    >
+      <Pre maxWidth={600} whiteSpace="pre-wrap" marginBottom={majorScale(2)}>
+        <Code appearance="minimal" size={500}>
+          {monitor.query}
+        </Code>
+      </Pre>
+      <Pane display="flex" justifyContent="flex-end">
+        <Button color="danger" appearance="minimal" onClick={close}>
+          Close
+        </Button>
+        <Button
+          appearance="minimal"
+          onClick={() => {
+            copy(monitor.query)
+            toaster.success(`Copied query from monitor ${monitor.name}`)
+            close()
+          }}
+        >
+          Copy Epl Query
+        </Button>
+      </Pane>
+    </Pane>
+  )
+}
+
 const ViewEplQueryCell = ({ monitor }) => {
   return (
     <Popover
       trigger="hover"
-      content={({ close }) => (
-        <Pane
-          width="auto"
-          height="auto"
-          padding={majorScale(2)}
-          background="tint1"
-          display="flex"
-          flexDirection="column"
-        >
-          <Pre
-            maxWidth={600}
-            whiteSpace="pre-wrap"
-            marginBottom={majorScale(2)}
-          >
-            <Code appearance="minimal" size={500}>
-              {monitor.query}
-            </Code>
-          </Pre>
-          <Pane display="flex" justifyContent="flex-end">
-            <Button color="danger" appearance="minimal" onClick={close}>
-              Close
-            </Button>
-            <Button
-              appearance="minimal"
-              onClick={() => {
-                copy(monitor.query)
-                toaster.success(`Copied query from monitor ${monitor.name}`)
-                close()
-              }}
-            >
-              Copy Epl Query
-            </Button>
-          </Pane>
-        </Pane>
-      )}
+      content={({ close }) => <Query monitor={monitor} close={close} />}
     >
       <Button appearance="minimal">View EPL Query</Button>
     </Popover>
