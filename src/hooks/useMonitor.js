850e980657c1035e6fbc911a0185bb51e75aee7a
Rob Phoenix <rob@robphoenix.com>
2019-07-15 10:39:30 +0100

    Manage monitor state in separate hook

---
+++ src/hooks/useMonitor.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { getMonitorById } from '../utils/monitors-client'
+
+const monitorsReducer = (state, action) => {
+  switch (action.type) {
+    case `SUCCESS`:
+      return {
+        ...state,
+        ...action.payload,
+        isLoading: false,
+        errors: {},
+      }
+    case `SET_ERROR`:
+      return {
+        ...state,
+        errors: {
+          ...state.errors,
+          ...action.payload,
+        },
+      }
+    default:
+      return state
+  }
+}
+
+const initialState = {
+  monitor: {},
+  isLoading: true,
+  errors: {},
+}
+
+const useMonitor = () => {
+  const [state, dispatch] = React.useReducer(monitorsReducer, initialState)
+
+  const fetchMonitorById = async (id) => {
+    try {
+      const monitor = await getMonitorById(id)
+      dispatch({ type: 'SUCCESS', payload: { monitor } })
+    } catch (error) {
+      dispatch({ type: 'SET_ERROR', payload: { monitorById: error } })
+    }
+  }
+
+  return {
+    fetchMonitorById,
+    ...state,
+  }
+}
+
+export default useMonitor
